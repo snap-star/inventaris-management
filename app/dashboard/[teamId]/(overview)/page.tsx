af5e586c577926b915660f4258d09a9fec73bb8f
snap-star <rendiyuspramana@gmail.com>
2024-10-17 13:55:16 +0000

    layouting table 2

--- app/dashboard/[teamId]/(overview)/page.tsx
+++ app/dashboard/[teamId]/(overview)/page.tsx
@@ -16,7 +16,7 @@ export default function DashboardPage() {
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-2xl font-bold tracking-tight">Data Barang</h2>
           </div>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <div>
               <InventoryOverview />
           </div>
         </div>
