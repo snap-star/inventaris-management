2d95e770126c2cd403a6bf5df9f435585461ff65
snap-star <rendiyuspramana@gmail.com>
2024-10-17 13:53:24 +0000

    layouting table

--- app/dashboard/[teamId]/(overview)/page.tsx
+++ app/dashboard/[teamId]/(overview)/page.tsx
@@ -14,15 +14,10 @@ export default function DashboardPage() {
       <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-2xl font-bold tracking-tight">Overview</h2>
+            <h2 className="text-2xl font-bold tracking-tight">Data Barang</h2>
           </div>
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            <CardHeader>
-              <CardTitle>Data Barang</CardTitle>
-            </CardHeader>
-            <CardContent className="col-span-3">
               <InventoryOverview />
-            </CardContent>
           </div>
         </div>
       </div>
