18e32f899844570e2be26881ff56300813d2f436
snap-star <rendiyuspramana@gmail.com>
2024-10-19 14:13:12 +0000

    maintenance page

--- app/dashboard/[teamId]/[placeholder]/page.tsx
+++ app/dashboard/[teamId]/[placeholder]/page.tsx
@@ -1,7 +1,15 @@
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+
 export default function Page() {
   return (
     <div>
-      <h1>Example Page</h1>
+      <Card>
+        <CardTitle>Maintenance</CardTitle>
+        <CardContent>
+          <p>Menu ini sedang dalam pengerjaan.</p>
+        </CardContent>
+      </Card>
+      <h1>Contoh Halaman</h1>
     </div>
   )
 }
